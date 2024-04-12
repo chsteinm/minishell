6711bfd3f75f3c41d371a78a2dfd6d854a34313e
MR0kernel <grodrigues991@gmail.com>
2024-04-12 12:07:36 +0200

    export passe testeur manque assignation 1 et free

--- srcs/export.c
+++ srcs/export.c
@@ -1,11 +1,13 @@
 #include "../includes/minishell.h"
 
 static char	*ft_get_env_name(t_data *data, char **str);
-static void	ft_parse_env(t_data *data, char *name, char *value);
+static int	ft_parse_env(char *name, char *value);
 static void	undo_split(t_list *node);
 void		ft_add_env(t_data *data, char *to_add);
 void		free_tab(char **tab);
 void		malloc_error(t_data *data);
+int			ft_isempty(char *str);
+
 
 void	ft_export(t_data *data, t_list *node)
 {
@@ -15,7 +17,11 @@ void	ft_export(t_data *data, t_list *node)
 	name = ft_get_env_name(data, &node->cmd[1]);
 	if (!name)
 		return ;
-	ft_parse_env(data, name, node->cmd[1]);
+	if (ft_parse_env(name, node->cmd[1]) == 1)
+	{
+		free(name);
+		return ;
+	}
 	if (ft_getenv(data->env, name))
 	{
 		ft_export_env(data, name, node->cmd[1]);
@@ -50,32 +56,48 @@ static void	undo_split(t_list *node)
 	node->cmd[1] = tmp;
 }
 
-static void	ft_parse_env(t_data *data, char *name, char *value)
+static int	ft_parse_env(char *name, char *value)
 {
-	// int		i;
-	// int		j;
-	// char	*tmp;
-
-	// i = 0;
-	// j = 0;
-	(void)data;
-	printf("name: %s\n", name);
-	printf("value: %s\n", value);
-
+	if (ft_isempty(name))
+	{
+		ft_putstr_fd("export: not a valid identifier\n", 2);
+		return (1);
+	}
+	while (*name)
+	{
+		if (ft_isalpha(*name) || *name == '_' || *name == '=')
+			name++;
+		else
+		{
+			ft_putstr_fd("export: not a valid identifier\n", 2);
+			return (1);
+		}
+	}
+	while (*value++)
+	{
+		if (!ft_isascii(*value++))
+		{
+			ft_putstr_fd("export: not a valid identifier\n", 2);
+			return (1);
+		}
+	}
+	return (0);
 }
-int	ft_strchr_(const char *str, int c)
+
+int	ft_isempty(char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == c)
-			return (1);
+		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i] != '=')
+			return (0);
 		i++;
 	}
-	return (0);
+	return (1);
 }
+
 static char	*ft_get_env_name(t_data *data, char **str)
 {
 	char	*name;
@@ -85,7 +107,7 @@ static char	*ft_get_env_name(t_data *data, char **str)
 	i = 0;
 	if (!*str)
 		return (NULL);
-	if (ft_strchr_(*str, '='))
+	if (ft_strchr(*str, '='))
 	{
 		while ((*str)[i] != '=')
 			i++;
