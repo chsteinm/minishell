d0de615b7b9d56de8a3d74b77362099bb60ef236
chsteinm <chris.steinmetz.69@gmail.com>
2024-04-17 22:27:31 +0200

    export ok with existing var

--- srcs/ft_export.c
+++ srcs/ft_export.c
@@ -1,6 +1,12 @@
 #include "../includes/minishell.h"
 
-void	insert_new_var
+void	update_var(t_data *data, t_list *node, int j_cmd, int j_env)
+{
+	free(data->env[j_env]);
+	data->env[j_env] = node->cmd[j_cmd];
+	node->cmd[j_cmd] = NULL;
+	printf("coucou\n");
+}
 
 void	ft_export(t_data *data, t_list *node)
 {
@@ -19,12 +25,13 @@ void	ft_export(t_data *data, t_list *node)
 		while (tmp[len_var] != '=')
 			if (!tmp[++len_var])
 				return ;
-		tmp[len_var + 1] = 0;
+		tmp[++len_var] = 0;
 		j_env = 0;
 		while (data->env[j_env] && !ft_strnstr(data->env[j_env], tmp, len_var))
 			j_env++;
+		printf("%zu %s\n", len_var, tmp);
 		free(tmp);
 		if (data->env[j_env])
-			return ();
+			return (update_var(data, node, j_cmd, j_env));
 	}
 }
